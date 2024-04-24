49d4f24bfe2b275b726a0e1f4bb949c52e941270
spiderkiley4 <spiderkiley4@gmail.com>
2024-04-24 08:58:54 -0400

    Fixed Movement Script
    Fixed movement script, collision mesh on player, added camera

--- player.gd
+++ player.gd
@@ -13,23 +13,23 @@ func _physics_process(delta):
 		
 	if Input.is_action_pressed("move_right"):
 		direction.x += 1
-		if Input.is_action_pressed("move_left"):
-			direction.x -= 1
-			if Input.is_action_pressed("move_back"):
-				direction.z += 1
-				if Input.is_action_pressed("move_forward"):
-					direction.z -= 1
-					
-					if direction != Vector3.ZERO:
-						direction = direction.normalized()
-						$Pivot.look_at(position + direction, Vector3.UP)
-						
-						# Ground Velocity
-						target_velocity.x = direction.x * speed
-						target_velocity.z = direction.z * speed
-						# Vertical Velocity
-						if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-							target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-							# Moving the Character
-							velocity = target_velocity
-							move_and_slide()
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.z += 1
+	if Input.is_action_pressed("move_up"):
+		direction.z -= 1
+		
+	#if direction != Vector3.ZERO:
+		#direction = direction.normalized()
+		#$Pivot.look_at(position + direction, Vector3.UP)
+		
+	# Ground Velocity
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	# Vertical Velocity
+	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	# Moving the Character
+	velocity = target_velocity
+	move_and_slide()
