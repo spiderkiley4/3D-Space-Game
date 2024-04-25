f174cda75986f0b5e231076a8c7b5a0cfd3bf5e6
spiderkiley4 <spiderkiley4@gmail.com>
2024-04-25 13:50:23 -0400

    Added enemy spawning

--- space.gd
+++ space.gd
@@ -1,10 +1,11 @@
-extends Node3D
+extends Node
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var enemy_scene: PackedScene
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_enemy_timer_timeout():
+	var enemy = enemy_scene.instantiate()
+	var enemy_spawn_location = get_node("SpawnPath/SpawnLocation")
+	enemy_spawn_location.progress_ratio = randf()
+	var player_position = $Player.position
+	enemy.initialize(enemy_spawn_location, player_position)
+	add_child(enemy)
