3718a908dd8fadf7a3abb5b43c30c4ba0f15b0b3
spiderkiley4 <spiderkiley4@gmail.com>
2024-04-25 14:31:31 -0400

    Unfinished movement and camera settings
    Made changes to movement and camera, not currently working

--- main.gd
+++ main.gd
@@ -7,5 +7,5 @@ func _on_enemy_timer_timeout():
 	var enemy_spawn_location = get_node("SpawnPath/SpawnLocation")
 	enemy_spawn_location.progress_ratio = randf()
 	var player_position = $Player.position
-	enemy.initialize(enemy_spawn_location, player_position)
+	enemy.initialize(enemy_spawn_location.position, player_position)
 	add_child(enemy)
--- player.gd
+++ player.gd
@@ -4,9 +4,12 @@ var projectile = preload("res://projectile.tscn")
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+@export var camera_rotation_sensitivity = 0.01
 
 var target_velocity = Vector3.ZERO
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
@@ -19,14 +22,18 @@ func _physics_process(delta):
 		direction.z += 1
 	if Input.is_action_pressed("move_up"):
 		direction.z -= 1
-		
-	#if direction != Vector3.ZERO:
-		#direction = direction.normalized()
-		#$Pivot.look_at(position + direction, Vector3.UP)
+	
+	var movement_direction = Vector3()
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(position + direction, Vector3.UP)
+		movement_direction = direction.rotated(Vector3.UP, $Camera.rotation.y)
 		
 	# Ground Velocity
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+	#target_velocity.x = direction.x * speed
+	#target_velocity.z = direction.z * speed
+	target_velocity.x = movement_direction.x * speed
+	target_velocity.z = movement_direction.z * speed
 	# Vertical Velocity
 	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
@@ -38,5 +45,15 @@ func fireprojectile():
 	var instance = projectile.instance() #unpacks the scene that is loaded in the preload function
 	instance.position = Vector3(0, 2, 0) #set whatever position you need
 	instance.linear_velocity = Vector3(0, 0, 0) #direction you want it to fire in
-	$MainSceneNode.add_child(instance) #adds child to the 3d world
+	$Main.add_child(instance) #adds child to the 3d world
 	print("spawned projectile")
+
+func _input(event):
+	#if Input.is_key_pressed(KEY_J):
+		#rotate_y(0.1)
+	if event is InputEventMouseMotion:
+		var camera_rotation = event.relative * camera_rotation_sensitivity
+		# "yaw" is the term for side-to-side turning of the camera (around a vertical axis)
+		rotate(Vector3.DOWN, camera_rotation.x)
+		# "pitch" is the term for up-and-down movement of the camera (around a horizontal axis)
+		rotate(Vector3.LEFT, camera_rotation.y)
